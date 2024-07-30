1808955866b4133fe6f6fb7182ec5bf003c483c7
alvin <alvin.sarisar@student.moringaschool.com>
2024-07-30 21:03:17 +0300

    complonent#

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
-
+import Navbar from "./Navbar";
+import Home from  "./Home";
+import About from "./About";
 function App() {
   return (
+    
     <div>
+      <Navbar />
+      <Home />
+      <About />
       <Article />
       <Comment />
     </div>
---
+++ src/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div>I'm a link!</div>;
+}
+
+export default Navbar;
